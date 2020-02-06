cfeebfdabd370a8e0eb083d68371573909bfdffa
Kresda <adserk@hotmail.com>
2020-02-06 15:58:09 +0000

    INTRO scene
    Added an intro template for the story.

---
+++ Space Education/Assets/Scripts/IntroScroll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroScroll : MonoBehaviour
+{
+    public float scrollSpeed = 20;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        Vector3 localVectorUp = transform.TransformDirection(0, 1, 0);
+
+        pos += localVectorUp * scrollSpeed * Time.deltaTime;
+        transform.position = pos;
+    }
+}
