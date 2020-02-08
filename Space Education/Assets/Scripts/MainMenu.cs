ffad9727ef76fb8c611af2963a9750b383f45931
Kresda <adserk@hotmail.com>
2020-02-08 15:16:08 +0000

    Font changes, credits screen

--- Space Education/Assets/Scripts/MainMenu.cs
+++ Space Education/Assets/Scripts/MainMenu.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
+
+
 {
  public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+// load the last scene
+ public void CreditsLoader()
+    {
+        SceneManager.LoadScene(SceneManager.sceneCountInBuildSettings-1);
+    }
+
  public void QuitGame()
     {
         Debug.Log("Quit");
         Application.Quit();
+    }
+
+    public AudioMixer audioMixer;
+ public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume",volume);
     }
 }
