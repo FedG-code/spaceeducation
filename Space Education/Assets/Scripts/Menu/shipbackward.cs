17b27e68e77ddd65aafa52431cb1681270ead75f
Kresda <adserk@hotmail.com>
2020-02-08 17:44:02 +0000

    Main menu vfx
    Added moving spaceships and planets to the main menu.
    And added distant stars to the intro scene.

---
+++ Space Education/Assets/Scripts/Menu/shipbackward.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shipbackward : MonoBehaviour
+{
+    //movement speed in units per second
+    public float movementspeed = 2;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        Vector3 localVectorUp = transform.InverseTransformDirection(0, 0, 1);
+
+        pos += localVectorUp * movementspeed * Time.deltaTime;
+        transform.position = pos;
+    }
+}
