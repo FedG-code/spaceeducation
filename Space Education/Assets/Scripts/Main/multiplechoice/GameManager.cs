bdabe889ca2280b30bab76987cea377ff1004cce
Kresda <adserk@hotmail.com>
2020-02-11 01:49:50 +0000

    Bug fix
    Fixed Question not showing up problem.

--- Space Education/Assets/Scripts/Main/multiplechoice/GameManager.cs
+++ Space Education/Assets/Scripts/Main/multiplechoice/GameManager.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public Question[] Questions { get; private set; } = null;
+    Question[] _questions = null;
+    public Question[] Questions { get { return _questions; } }
 
     [SerializeField] GameEvents events = null;
 
@@ -16,14 +17,14 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        Display();
+        
 
         LoadQuestions();
         foreach (var question in Questions)
         {
             Debug.Log(question.Info);
         }
-
+        Display();
     }
 
     public void EraseAnswers()
@@ -54,7 +55,7 @@ public class GameManager : MonoBehaviour
     int GetRandomQuestionIndex()
     {
         var random = 0;
-        if (FinishedQuestions.Count<Questions.Length)
+        if (FinishedQuestions.Count < Questions.Length)
         {
             do
             {
@@ -69,10 +70,10 @@ public class GameManager : MonoBehaviour
     {
         // Load all the questions inside the 'Resources'/'Questions' folder
         Object[] objs = Resources.LoadAll("Questions", typeof(Question));
-        Questions = new Question[objs.Length];
+        _questions = new Question[objs.Length];
         for (int i = 0; i< objs.Length; i++)
         {
-            Questions[i] = (Question)objs[i];
+            _questions[i] = (Question)objs[i];
         }
     }
 
