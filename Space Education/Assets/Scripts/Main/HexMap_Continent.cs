03f8523aae0b03de96b836601d652657af9a5562
Samuel Overington <s.c.overington@gmail.com>
2020-02-27 19:25:04 +0000

    Changing format from string to enum of tiletypes

--- Space Education/Assets/Scripts/Main/HexMap_Continent.cs
+++ Space Education/Assets/Scripts/Main/HexMap_Continent.cs
@@ -9,44 +9,44 @@ public class HexMap_Continent : HexMap
         base.GenerateMap();
 
         //small sun
-        GenerateTile(8, 23, 2, "star");
+        GenerateTile(8, 23, 2,HEX.TILE_TYPE.STAR);
         //planet tile
-        GenerateTile(8, 26, 1, "planet");
+        GenerateTile(8, 26, 1,Hex.TILE_TYPE.PLANET);
 
-        GenerateTile(6, 30, 1, "planet");
+        GenerateTile(6, 30, 1,Hex.TILE_TYPE.PLANET);
 
-        GenerateTile(12, 29, 1, "planet");
+        GenerateTile(12, 29, 1,Hex.TILE_TYPE.PLANET);
 
-        GenerateTile(15, 24, 1, "event");
+        GenerateTile(15, 24, 1,Hex.TILE_TYPE.EVENT);
 
-        GenerateTile(17, 32, 3, "black hole");
+        GenerateTile(17, 32, 3,Hex.TILE_TYPE.BLACKHOLE);
 
-        GenerateTile(5,32, 1, "asteroid");
+        GenerateTile(5 ,32, 1,Hex.TILE_TYPE.ASTEROID);
 
-        GenerateTile (5,33, 1, "asteroid");
+        GenerateTile(5,33, 1,Hex.TILE_TYPE.ASTEROID);
 
-        GenerateTile(6,33, 1, "asteroid");
+        GenerateTile(6,33, 1,Hex.TILE_TYPE.ASTEROID);
 
-        GenerateTile(6, 34, 1, "asteroid");
+        GenerateTile(6, 34, 1,Hex.TILE_TYPE.ASTEROID);
 
-        GenerateTile(7,34, 1, "asteroid");
+        GenerateTile(7,34, 1,Hex.TILE_TYPE.ASTEROID);
 
-        GenerateTile(8, 34, 1, "asteroid");
+        GenerateTile(8, 34, 1,Hex.TILE_TYPE.ASTEROID);
 
-        GenerateTile(9, 33, 1, "asteroid");
+        GenerateTile(9, 33, 1,Hex.TILE_TYPE.ASTEROID);
 
-        GenerateTile(10, 33, 1, "asteroid");
+        GenerateTile(10, 33, 1,Hex.TILE_TYPE.ASTEROID);
 
-        GenerateTile(11, 33, 1, "asteroid");
+        GenerateTile(11, 33, 1,Hex.TILE_TYPE.ASTEROID);
 
-        GenerateTile(12, 33, 1, "asteroid");
+        GenerateTile(12, 33, 1,Hex.TILE_TYPE.ASTEROID);
 
-        GenerateTile(12, 34, 1, "asteroid");
+        GenerateTile(12, 34, 1,Hex.TILE_TYPE.ASTEROID);
 
-        GenerateTile(13, 34, 1, "asteroid");
-        //Debug.LogError("override ok");
+        GenerateTile(13, 34, 1,Hex.TILE_TYPE.ASTEROID);
+        //Debug.LogError(override ok);
 
-        //Fed: here comes the skipping we're not concerned with meshes 
+        //Fed: here comes the skipping we're not concerned with meshes
         UpdateHexVisuals();
     }
     //Fed: what quill calls elevate area I called Generate Tile
@@ -56,7 +56,7 @@ public class HexMap_Continent : HexMap
 
         Hex centerHex = GetHexAt(q, r);
         //centerHex.Elevation = 1f;
-        
+
         //Debug.LogError("Generate sun okay");
         Hex[] areaHexes = GetHexesWithinRadiusOf(centerHex, range);
 
@@ -64,7 +64,7 @@ public class HexMap_Continent : HexMap
         {
             h.tiletype = material;
         }
-        
+
     }
-    
+
 }
