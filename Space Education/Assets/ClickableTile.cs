0033f659ce98112db458f537dfcb92d474cf918d
Samuel Overington <s.c.overington@gmail.com>
2020-03-13 17:43:25 +0000

    add box colider for hexprefab, and make each hex clickable

---
+++ Space Education/Assets/ClickableTile.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableTile : MonoBehaviour
+{
+    void OnMouseUp() {
+        Debug.Log("Clicko World!");
+    }
+}
