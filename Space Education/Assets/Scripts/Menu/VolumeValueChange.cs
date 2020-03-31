4305e55786e58fc22aa7d4f4429dd17480200430
Kresda <adserk@hotmail.com>
2020-04-01 00:14:10 +0100

    volume slider
    volume slider controls the BG volume now

---
+++ Space Education/Assets/Scripts/Menu/VolumeValueChange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeValueChange : MonoBehaviour
+{
+
+  private AudioSource AudioSource;
+
+  private float musicVolume = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioSource.volume = musicVolume;
+    }
+
+    public void SetVolume (float vol)
+    {
+      musicVolume = vol;
+    }
+}
