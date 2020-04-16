5471d1d882cdc832ffd3ee6c1ca9ee23235bd10e
Kresda <adserk@hotmail.com>
2020-04-16 12:34:35 +0100

    Minimap issues

---
+++ Space Education/Assets/Scripts/Main/MiniMap2.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap2: MonoBehaviour
+{
+    // Start is called before the first frame update
+
+        public Transform player;
+        public HexMap hexmap;
+
+    void Start()
+    {
+      player = hexmap.selectedUnit;
+    }
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }
+}
